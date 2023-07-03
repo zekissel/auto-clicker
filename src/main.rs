a5dd06b824b075887fd2cb79a909a409a7558586
Zane Kissel <zekissel@gmail.com>
2023-07-03 00:31:54 -0400

    add GUI components

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,37 @@ use mki::{bind_key, Action, Keyboard };
 use std::sync::RwLock;
 use std::thread;
 use std::time::Duration;
+use imgui::*;
+
+mod support;
 
 #[macro_use]
 extern crate lazy_static;
 
 lazy_static! {
     static ref ACTIVE: RwLock<bool> = RwLock::new(false);
+    static ref BUTTON: RwLock<bool> = RwLock::new(true);
 }
 
+
+fn click () {
+
+    toggle();
+
+    let side: bool = BUTTON.read().unwrap().to_owned();
+
+    while ACTIVE.read().unwrap().to_owned() {
+
+        if side {
+            mki::Mouse::release(&mki::Mouse::Left);
+            mki::Mouse::release(&mki::Mouse::Left); 
+        }
+        else { mki::Mouse::release(&mki::Mouse::Right); }
+
+        if side { thread::sleep(Duration::from_millis(1)) } 
+        else { thread::sleep(Duration::from_millis(500)) }
+    }
+}
 
 fn toggle () {
     let act: bool = ACTIVE.read().unwrap().to_owned();
@@ -17,16 +40,51 @@ fn toggle () {
     *new_act = !act;
 }
 
+fn toggle_button () {
+    let but: bool = BUTTON.read().unwrap().to_owned();
+    let mut new_but = BUTTON.write().unwrap();
+    *new_but = !but;
+}
+
 fn main () {
 
-    bind_key(Keyboard::G, Action::handle_kb(|_| toggle() ));
+    let system = support::init(file!());
+    let mut value = 0;
+    let start = ["Start(G)", "Stop(G)"];
+    let mut value2 = 0;
+    let button = ["Left Click", "Right Click"];
+
+    bind_key(Keyboard::G, Action::handle_kb(|_| { click() } ));
     bind_key(Keyboard::Q, Action::handle_kb(|_| std::process::exit(0) ));
-    println!("Use the <G> key to start/stop clicker. \nUse the <Q> key to exit to terminal.");
+    println!("\nUse the <G> key to start/stop clicker. \nUse the <Q> key to exit to terminal.\n");
 
-    loop {
 
-        if ACTIVE.read().unwrap().to_owned() { mki::Mouse::release(&mki::Mouse::Left); }
+    system.main_loop(move |_, ui| {
+        ui.window("Auto-Clicker")
+            .size([500.0, 360.0], Condition::FirstUseEver)
+            .build(|| {
+                ui.text_wrapped("Use your keyboard to start/stop clicking when this window is not in focus");
+                if ui.button(start[value]) {
+                    value += 1;
+                    value %= 2;
+                    click();
+                }
+                ui.separator();
 
-        thread::sleep(Duration::from_millis(1));
-    }
+                if ui.button(button[value2]) {
+                    value2 += 1;
+                    value2 %= 2;
+                    toggle_button();
+                }
+
+                ui.separator();
+
+                let mouse_pos = ui.io().mouse_pos;
+                ui.text(format!(
+                    "Mouse Position: ({:.1},{:.1})",
+                    mouse_pos[0], mouse_pos[1]
+                ));
+            });
+    });
+    
 }
